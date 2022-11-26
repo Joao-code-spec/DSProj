1aa6e4ed8b40debbc42cedcb9f31880d6a51455d
DiogoFaustino1 <diogovicentefaustino@tecnico.ulisboa.pt>
2022-11-26 17:30:46 +0000

    sparsity para os dois datasets

--- sparsity_test.py
+++ sparsity_test.py
@@ -1,4 +1,5 @@
 import matplotlib
+import pandas
 from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
 from matplotlib.pyplot import subplots, savefig, show, figure, title
@@ -9,9 +10,6 @@ matplotlib.use('Agg')
 register_matplotlib_converters()
 filename = 'data/drought.csv'
 data = read_csv(filename, index_col='date',parse_dates=True, infer_datetime_format=True)
-
-data.head()
-data.shape
 
 numeric_vars = get_variable_types(data)['Numeric']
 if [] == numeric_vars:
@@ -24,7 +22,7 @@ corr_mtx = abs(data.corr())
 fig = figure(figsize=(cols*HEIGHT, rows*HEIGHT))
 heatmap(abs(corr_mtx), xticklabels=corr_mtx.columns, yticklabels=corr_mtx.columns, annot=True, cmap='Blues')
 title('Correlation analysis')
-savefig(f'images/correlation_analysis.png')
+savefig(f'images/correlation_analysis_drought.png')
 show()
 
 
@@ -37,7 +35,35 @@ for i in range(len(numeric_vars)):
         axs[i, j-1].set_xlabel(var1)
         axs[i, j-1].set_ylabel(var2)
         axs[i, j-1].scatter(data[var1], data[var2])
-savefig(f'images/sparsity_study_2_numeric.png')
+savefig(f'images/sparsity_study_drought_numeric.png')
 show()
 
 
+filename = 'data/diabetic_data.csv'
+data_2 = read_csv(filename)
+numeric_vars = get_variable_types(data_2)['Numeric']
+if [] == numeric_vars:
+    raise ValueError('There are no numeric variables.')
+
+rows, cols = len(numeric_vars)-1, len(numeric_vars)-1
+
+corr_mtx_2 = abs(data_2.corr())
+
+fig = figure(figsize=(cols*HEIGHT, rows*HEIGHT))
+heatmap(abs(corr_mtx_2), xticklabels=corr_mtx_2.columns, yticklabels=corr_mtx_2.columns, annot=True, cmap='Blues')
+title('Correlation analysis')
+savefig(f'images/correlation_analysis_diabetic.png')
+show()
+
+
+fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
+for i in range(len(numeric_vars)):
+    var1 = numeric_vars[i]
+    for j in range(i+1, len(numeric_vars)):
+        var2 = numeric_vars[j]
+        axs[i, j-1].set_title("%s x %s"%(var1,var2))
+        axs[i, j-1].set_xlabel(var1)
+        axs[i, j-1].set_ylabel(var2)
+        axs[i, j-1].scatter(data_2[var1], data_2[var2])
+savefig(f'images/sparsity_study_diabetic_numeric.png')
+show()
