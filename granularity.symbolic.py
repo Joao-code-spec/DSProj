a3fee0434ecc7b9e11205e137e1392de0b181c8a
ZeIsabel <ze.889@live.com.pt>
2022-11-28 13:56:08 +0000

    alteracoes ao granularity symbolic

--- granularity.symbolic.py
+++ granularity.symbolic.py
@@ -1,7 +1,7 @@
 from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
+from ds_charts import get_variable_types, choose_grid, HEIGHT
 from matplotlib.pyplot import subplots, savefig, show
-from ds_charts import get_variable_types, HEIGHT
 from matplotlib.pyplot import savefig, show, subplots
 from ds_charts import HEIGHT, get_variable_types
 
@@ -15,13 +15,14 @@ symbolic_vars = get_variable_types(data)['Symbolic']
 if [] == symbolic_vars:
     raise ValueError('There are no symbolic variables.')
 
-rows, cols = len(symbolic_vars), 3
+rows, cols = choose_grid(len(symbolic_vars))
 fig, axs = subplots(rows, cols, figsize=(cols*HEIGHT, rows*HEIGHT), squeeze=False)
-for i in range(len(symbolic_vars)):
-    for j in range(3):
-        axs[i, j].set_title("Histogram")
-        axs[i, j].set_xlabel(symbolic_vars[i])
-        axs[i, j].set_ylabel('nr records')
-        axs[i, j].hist(data[symbolic_vars[i]], data.shape[0])
-savefig(f'images/sparsity_study_symbolic.png')
+i, j = 0, 0
+for n in range(len(symbolic_vars)):
+    axs[i, j].set_title('Histogram for %s'%symbolic_vars[n])
+    axs[i, j].set_xlabel(symbolic_vars[n])
+    axs[i, j].set_ylabel('nr records')
+    axs[i, j].scatter(symbolic_vars[n], data.shape[0])
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+savefig('images/granularity_symbolic.png')
 show()
