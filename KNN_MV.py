b63d21da0f9a71336f4d1d22d25f36c69a23a5cd
ZeIsabel <ze.889@live.com.pt>
2022-12-04 22:04:08 +0000

    update

--- KNN_MV.py
+++ KNN_MV.py
@@ -53,15 +53,54 @@ savefig('images/{file_tag}_knn_study.png')
 show()
 print('Best results with %d neighbors and %s'%(best[0], best[1]))
 
-###############
+############### confusion matrix
 
-clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+def plot_confusion_matrix(cnf_matrix: np.ndarray, classes_names: np.ndarray, ax: plt.Axes = None,
+                          normalize: bool = False):
+    if ax is None:
+        ax = plt.gca()
+    if normalize:
+        total = cnf_matrix.sum(axis=1)[:, np.newaxis]
+        cm = cnf_matrix.astype('float') / total
+        title = "Normalized confusion matrix"
+    else:
+        cm = cnf_matrix
+        title = 'Confusion matrix'
+    np.set_printoptions(precision=2)
+    tick_marks = np.arange(0, len(classes_names), 1)
+    ax.set_title(title)
+    ax.set_ylabel('True label')
+    ax.set_xlabel('Predicted label')
+    ax.set_xticks(tick_marks)
+    ax.set_yticks(tick_marks)
+    ax.set_xticklabels(classes_names)
+    ax.set_yticklabels(classes_names)
+    ax.imshow(cm, interpolation='nearest', cmap=CMAP)
+
+    fmt = '.2f' if normalize else 'd'
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], fmt), color='y', horizontalalignment="center")
+
+############### chamar a funcao da confusion matrix
+
+data = read_csv('data/iris.csv')
+y = data.pop('readmitted').values
+X = data.values
+labels = unique(y)
+labels.sort()
+
+trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
+
+clf = GaussianNB()
 clf.fit(trnX, trnY)
-prd_trn = clf.predict(trnX)
-prd_tst = clf.predict(tstX)
-plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
-savefig('images/{file_tag}_knn_best.png')
-show()
+prdY = clf.predict(tstX)
+
+plt.figure()
+fig, axs = plt.subplots(1, 2, figsize=(8, 4), squeeze=False)
+plot_confusion_matrix(confusion_matrix(tstY, prdY, labels=labels), labels, ax=axs[0,0], )
+plot_confusion_matrix(confusion_matrix(tstY, prdY, labels=labels), labels, ax=axs[0,1], normalize=True)
+plt.tight_layout()
+plt.show()
 
 ###############
 
@@ -85,3 +124,13 @@ for n in nvalues:
     y_tst_values.append(eval_metric(tstY, prd_tst_Y))
     y_trn_values.append(eval_metric(trnY, prd_trn_Y))
 plot_overfitting_study(nvalues, y_trn_values, y_tst_values, name=f'KNN_K={n}_{d}', xlabel='K', ylabel=str(eval_metric))
+
+############### 
+
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig('images/{file_tag}_knn_best.png')
+show()
