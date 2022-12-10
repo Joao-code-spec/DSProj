75fe0925e33a76f544efd8c04d1b3838fb762779
ZeIsabel <ze.889@live.com.pt>
2022-12-10 14:25:23 +0000

    update

--- Test and Train.py
+++ Test and Train.py
@@ -8,21 +8,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import recall_score, confusion_matrix, accuracy_score,precision_score
 from sklearn.neighbors import KNeighborsClassifier
 
-x=0 #0 para drop; usar outro numero se quiser usar o truncate
+#x=3 se diabetico x=2 se drought
+x=3
 
-if x==0:
-    data: DataFrame = read_csv('data/outliers/my_diabetic_data_drop_outliers.csv')
-    file_tag = 'OutlierDrop_diabetic_mean'
-else:
-    data: DataFrame = read_csv('data/outliers/my_diabetic_data_truncate_outliers.csv')
-    file_tag = 'OutlierTruncate_diabetic_mean'
+#ALTERAR FICHEIRO
+data: DataFrame = read_csv('data/diabetic_data.csv')
+file_tag = 'OutlierDrop_drought'
 
+if x==3:
 #para dados diabeticos
-target = 'readmitted'
-verypositive = 2
-positive = 1
-negative = 0
-values = {'Original': [len(data[data[target] == verypositive]), len(data[data[target] == positive]), len(data[data[target] == negative])]}
+    target = 'readmitted'
+    verypositive = 2
+    positive = 1
+    negative = 0
+    values = {'Original': [len(data[data[target] == verypositive]), len(data[data[target] == positive]), len(data[data[target] == negative])]}
+else:
+    #para dados drought
+    target = 'Class'
+    positive = 1
+    negative = 0
+    values = {'Original': [len(data[data[target] == positive]), len(data[data[target] == negative])]}
 
 y: np.ndarray = data.pop(target).values
 X: np.ndarray = data.values
@@ -31,14 +36,20 @@ labels.sort()
 trnX, tstX, trnY, tstY = train_test_split(X, y, train_size=0.7, stratify=y)
 
 train = concat([DataFrame(trnX, columns=data.columns), DataFrame(trnY,columns=[target])], axis=1)
-train.to_csv(f'data/outliers/out/{file_tag}_train.csv', index=False)
+train.to_csv(f'data/file_tag}_train.csv', index=False)
 
 test = concat([DataFrame(tstX, columns=data.columns), DataFrame(tstY,columns=[target])], axis=1)
-test.to_csv(f'data/outliers/out/{file_tag}_test.csv', index=False)
+test.to_csv(f'data/{file_tag}_test.csv', index=False)
 
-values['Train'] = [len(np.delete(trnY, np.argwhere(trnY!=verypositive))), len(np.delete(trnY, np.argwhere(trnY!=positive))),len(np.delete(trnY, np.argwhere(trnY!=negative)))]
-values['Test'] = [len(np.delete(tstY, np.argwhere(tstY!=verypositive))), len(np.delete(tstY, np.argwhere(tstY!=positive))),len(np.delete(tstY, np.argwhere(tstY!=negative)))]
+if x==3:
+    #para dados diabeticos
+    values['Train'] = [len(np.delete(trnY, np.argwhere(trnY!=verypositive))), len(np.delete(trnY, np.argwhere(trnY!=positive))),len(np.delete(trnY, np.argwhere(trnY!=negative)))]
+    values['Test'] = [len(np.delete(tstY, np.argwhere(tstY!=verypositive))), len(np.delete(tstY, np.argwhere(tstY!=positive))),len(np.delete(tstY, np.argwhere(tstY!=negative)))]
+else:
+    #para dados drought
+    values['Train'] = [len(np.delete(trnY, np.argwhere(trnY==negative))), len(np.delete(trnY, np.argwhere(trnY==positive)))]
+    values['Test'] = [len(np.delete(tstY, np.argwhere(tstY==negative))), len(np.delete(tstY, np.argwhere(tstY==positive)))]
 
-#plt.figure(figsize=(12,4))
-#ds.multiple_bar_chart([verypositive, positive, negative], values, title='Data distribution per dataset')
-#plt.savefig('images/classification/diabetic_IterativeImputer_readmitted_distribution.png')
+plt.figure(figsize=(12,4))
+ds.multiple_bar_chart([verypositive, positive, negative], values, title='Data distribution per dataset')
+plt.savefig('images/distribution.png')
