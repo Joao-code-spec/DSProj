c880bdd3fe3bd2d9ed88365fcdea8e1461a9c993
cat99cat <catarina.g.santos@tecnico.ulisboa.pt>
2022-12-11 02:01:09 +0000

    data

--- Scaling_diabetic.py
+++ Scaling_diabetic.py
@@ -3,7 +3,7 @@ from pandas.plotting import register_matplotlib_converters
 
 register_matplotlib_converters()
 file = 'my_diabetic_data'
-filename = 'data/my_diabetic_data.csv'
+filename = 'data/outliers/my_diabetic_data_drop_outliers.csv'
 data = read_csv(filename, na_values='', parse_dates=True, infer_datetime_format=True)
 
 from ds_charts import get_variable_types
@@ -25,7 +25,7 @@ from pandas import DataFrame, concat
 transf = StandardScaler(with_mean=True, with_std=True, copy=True).fit(df_nr)
 tmp = DataFrame(transf.transform(df_nr), index=data.index, columns= numeric_vars)
 norm_data_zscore = concat([tmp, df_sb,  df_bool], axis=1)
-norm_data_zscore.to_csv(f'data/{file}_scaled_zscore.csv', index=False)
+norm_data_zscore.to_csv(f'data/Scaling/{file}_scaled_zscore.csv', index=False)
 
 #MinMaxScaler
 
@@ -35,12 +35,12 @@ from pandas import DataFrame, concat
 transf = MinMaxScaler(feature_range=(0, 1), copy=True).fit(df_nr)
 tmp = DataFrame(transf.transform(df_nr), index=data.index, columns= numeric_vars)
 norm_data_minmax = concat([tmp, df_sb,  df_bool], axis=1)
-norm_data_minmax.to_csv(f'data/{file}_scaled_minmax.csv', index=False)
+norm_data_minmax.to_csv(f'data/Scaling/{file}_scaled_minmax.csv', index=False)
 print(norm_data_minmax.describe())
 
 #plots
 
-from matplotlib.pyplot import subplots, show
+from matplotlib.pyplot import subplots, show, savefig
 
 fig, axs = subplots(1, 3, figsize=(20,10),squeeze=False)
 axs[0, 0].set_title('Original data')
@@ -49,4 +49,6 @@ axs[0, 1].set_title('Z-score normalization')
 norm_data_zscore.boxplot(ax=axs[0, 1])
 axs[0, 2].set_title('MinMax normalization')
 norm_data_minmax.boxplot(ax=axs[0, 2])
+
+savefig('images/Scaling/global_boxplot_'+file+'.png')
 show()
