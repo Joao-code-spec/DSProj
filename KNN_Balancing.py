e36481e08a412107a1a20f7e40c0db546a5df75b
Joao-code-spec <72688386+Joao-code-spec@users.noreply.github.com>
2022-12-14 21:32:52 +0000

    diabetic knn balance code

--- KNN_Balancing.py
+++ KNN_Balancing.py
@@ -3,9 +3,9 @@ from pandas import DataFrame, read_csv, unique
 from matplotlib.pyplot import figure, savefig, show
 from sklearn.neighbors import KNeighborsClassifier
 from ds_charts import plot_evaluation_results, multiple_line_chart, plot_overfitting_study
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score , precision_score, recall_score, f1_score
 
-file_tag = 'diabetes_acc'
+file_tag = 'diabetes_f1'
 filename = 'data/Balancing/diabetic_oversample'
 target = 'readmitted'
 
@@ -19,8 +19,8 @@ test: DataFrame = read_csv(f'{filename}_test.csv')
 tstY: ndarray = test.pop(target).values
 tstX: ndarray = test.values
 
-eval_metric = accuracy_score
-nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 22, 25]
+eval_metric = f1_score
+nvalues = nvalues = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 22, 25, 27, 30, 32, 35, 36, 37, 39, 40]
 dist = ['manhattan', 'euclidean', 'chebyshev']
 values = {}
 best = (0, '')
@@ -28,17 +28,27 @@ last_best = 0
 for d in dist:
     y_tst_values = []
     for n in nvalues:
+        print("Cal_best" + n)
         knn = KNeighborsClassifier(n_neighbors=n, metric=d)
         knn.fit(trnX, trnY)
         prd_tst_Y = knn.predict(tstX)
-        y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+        y_tst_values.append(eval_metric(tstY, prd_tst_Y,average="macro"))
         if y_tst_values[-1] > last_best:
             best = (n, d)
             last_best = y_tst_values[-1]
     values[d] = y_tst_values
 
 figure()
-multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel=str(accuracy_score), percentage=True)
-savefig('images/balancing/KNN/diabetic/oversample/{file_tag}_knn_study.png')
+multiple_line_chart(nvalues, values, title='KNN variants', xlabel='n', ylabel=str(f1_score), percentage=True)
+savefig('images/balancing/KNN/diabetic/oversample/' + file_tag + '_knn_study.png')
 show()
 print('Best results with %d neighbors and %s'%(best[0], best[1]))
+"""
+clf = knn = KNeighborsClassifier(n_neighbors=best[0], metric=best[1])
+clf.fit(trnX, trnY)
+prd_trn = clf.predict(trnX)
+prd_tst = clf.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig(f'images/balancing/KNN/diabetic/oversample/{file_tag}_knn_best.png')
+show()
+"""
