6d9026cf3a48decc6ad3fb6f1227b8032a0a754c
cat99cat <catarina.g.santos@tecnico.ulisboa.pt>
2023-01-03 13:02:30 +0000

    NN

--- NN.py
+++ NN.py
@@ -2,12 +2,19 @@ from numpy import ndarray
 from pandas import DataFrame, read_csv, unique
 from matplotlib.pyplot import figure, subplots, savefig, show
 from sklearn.neural_network import MLPClassifier
-from ds_charts import plot_evaluation_results, multiple_line_chart, horizontal_bar_chart, HEIGHT
+from ds_charts import plot_evaluation_results, multiple_line_chart, horizontal_bar_chart, HEIGHT, plot_overfitting_study
 from sklearn.metrics import accuracy_score
 
-file_tag = 'diabetic'
-filename = 'data/Balancing/diabetic_undersample'
-target = 'readmitted'
+dataset = 'drought'
+
+if dataset == 'diabetic':
+    file_tag = 'diabetic'
+    filename = 'data/Balancing/diabetic_undersample'
+    target = 'readmitted'
+else:
+    file_tag = 'drought'
+    filename = 'data/Balancing/drought_undersample'
+    target = 'class'
 
 train: DataFrame = read_csv(f'{filename}_train.csv')
 trnY: ndarray = train.pop(target).values
@@ -35,6 +42,7 @@ for k in range(len(lr_type)):
     for lr in learning_rate:
         yvalues = []
         for n in max_iter:
+            print(n)
             mlp = MLPClassifier(activation='logistic', solver='sgd', learning_rate=d,
                                 learning_rate_init=lr, max_iter=n, verbose=False)
             mlp.fit(trnX, trnY)
@@ -50,3 +58,27 @@ for k in range(len(lr_type)):
 savefig(f'images/MLP/{file_tag}_mlp_study.png')
 show()
 print(f'Best results with lr_type={best[0]}, learning rate={best[1]} and {best[2]} max iter, with accuracy={last_best}')
+
+prd_trn = best_model.predict(trnX)
+prd_tst = best_model.predict(tstX)
+plot_evaluation_results(labels, trnY, prd_trn, tstY, prd_tst)
+savefig(f'images/MLP/{file_tag}_mlp_best.png')
+show()
+
+
+
+lr_type = 'adaptive'
+lr = 0.9
+eval_metric = accuracy_score
+y_tst_values = []
+y_trn_values = []
+for n in max_iter:
+    mlp = MLPClassifier(activation='logistic', solver='sgd', learning_rate=lr_type, learning_rate_init=lr, max_iter=n, verbose=False)
+    mlp.fit(trnX, trnY)
+    prd_tst_Y = mlp.predict(tstX)
+    prd_trn_Y = mlp.predict(trnX)
+    y_tst_values.append(eval_metric(tstY, prd_tst_Y))
+    y_trn_values.append(eval_metric(trnY, prd_trn_Y))
+plot_overfitting_study(max_iter, y_trn_values, y_tst_values, name=f'NN_lr_type={lr_type}_lr={lr}', xlabel='nr episodes', ylabel=str(eval_metric))
+savefig(f'images/MLP/{file_tag}_overfitting.png')
+show()
