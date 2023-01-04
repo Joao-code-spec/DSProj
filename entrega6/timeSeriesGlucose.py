c287e2002d89d939a69f3196f5dc5d2ea8116975
Joao-code-spec <72688386+Joao-code-spec@users.noreply.github.com>
2023-01-04 19:55:27 +0000

    created shorter glucose DATE and TARGET

--- entrega6/timeSeriesGlucose.py
+++ entrega6/timeSeriesGlucose.py
@@ -2,7 +2,7 @@ from pandas import read_csv
 from matplotlib.pyplot import figure, xticks, savefig, show
 from ts_functions import plot_series, HEIGHT
 
-data = read_csv('entrega6/data/glucoseMRows.csv', index_col='Date', sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+data = read_csv('data/glucoseDateTarget.csv', index_col='Date', sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
 
 data.sort_values('Date', axis=0, ascending=True, inplace=True, kind='quicksort', na_position='last', ignore_index=False, key=None)
 print("Nr. Records = ", data.shape[0])
@@ -11,4 +11,12 @@ print("Last timestamp", data.index[-1])
 figure(figsize=(3*HEIGHT, HEIGHT))
 plot_series(data, x_label='Date', y_label='Glucose', title='Glucose')
 xticks(rotation = 45)
-savefig('entrega6/images/glucoseMRows.png')
+savefig('images/glucose/GlucoseHour.png')
+show()
+
+day_df = data.copy().groupby(data.index.date).mean()
+figure(figsize=(3*HEIGHT, HEIGHT))
+plot_series(day_df, title='Daily consumptions', x_label='timestamp', y_label='consumption')
+xticks(rotation = 45)
+savefig('images/glucose/glucoseDay.png')
+show()
