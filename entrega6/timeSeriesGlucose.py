0f38e2d18ad3f7ce4004937264e0a091705f8fa1
DiogoFaustino1 <diogovicentefaustino@tecnico.ulisboa.pt>
2023-01-04 14:49:29 +0000

    timeSeries Glucose resolvido

--- entrega6/timeSeriesGlucose.py
+++ entrega6/timeSeriesGlucose.py
@@ -2,12 +2,13 @@ from pandas import read_csv
 from matplotlib.pyplot import figure, xticks, savefig, show
 from ts_functions import plot_series, HEIGHT
 
-data = read_csv('data/glucoseMRows.csv', index_col='Date', sep=',', decimal='.', parse_dates=True, infer_datetime_format=True)
+data = read_csv('entrega6/data/glucoseMRows.csv', index_col='Date', sep=',', decimal='.', parse_dates=True, infer_datetime_format=True, dayfirst=True)
+
+data.sort_values('Date', axis=0, ascending=True, inplace=True, kind='quicksort', na_position='last', ignore_index=False, key=None)
 print("Nr. Records = ", data.shape[0])
 print("First timestamp", data.index[0])
 print("Last timestamp", data.index[-1])
 figure(figsize=(3*HEIGHT, HEIGHT))
 plot_series(data, x_label='Date', y_label='Glucose', title='Glucose')
 xticks(rotation = 45)
-savefig('images/glucoseMRows.png')
-show()
+savefig('entrega6/images/glucoseMRows.png')
